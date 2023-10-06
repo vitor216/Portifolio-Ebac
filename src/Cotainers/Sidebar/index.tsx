5b295bd36b24ad7ab69a7604ab36359dc82cdfd5
vitor <eletrotecvitor@gmail.com>
2023-10-05 22:04:28 -0300

    estilizando a sidebar

--- src/Cotainers/Sidebar/index.tsx
+++ src/Cotainers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 const Sidebar = () => (
   <aside>
-    <img src="Https://github.com/vitor216.png" />
-    <Titulo fontSize={20}>Vitor de Aguiar</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Vitor de Aguiar</Titulo>
+      <Paragrafo tipo="secundario" fontSise={16}>
+        vitor216
+      </Paragrafo>
+      <Descricao tipo="principal" fontSise={12}>
+        Analista d Sistemas
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
--- src/components/Paragrafo/index.tsx
+++ src/components/Paragrafo/index.tsx
@@ -3,10 +3,13 @@ import { P } from './styles'
 export type Props = {
   children: string
   tipo?: 'principal' | 'secundario'
+  fontSise?: number
 }
 
-const Paragrafo = ({ children, tipo = 'principal' }: Props) => (
-  <P tipo={tipo}>{children}</P>
+const Paragrafo = ({ children, tipo = 'principal', fontSise }: Props) => (
+  <P fontSise={fontSise} tipo={tipo}>
+    {children}
+  </P>
 )
 
 export default Paragrafo
